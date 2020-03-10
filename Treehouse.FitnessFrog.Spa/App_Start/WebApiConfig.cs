40d08d1078c9afd5ee84f0cef75831f613750f0c
forati89 <has_allak@yahoo.com>
2020-03-11 00:08:44 +0100

    Add project files.

---
+++ Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace Treehouse.FitnessFrog.Spa
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // configuration for DI to ignore Reference loop
+            var jsonSerializerSettings = config.Formatters.JsonFormatter.SerializerSettings;
+            jsonSerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            // converting models to camelCase instead of PasCalCase in order to js to be able to consume data
+            jsonSerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}", defaults: new {id = RouteParameter.Optional});
+        }
+    }
+}
